00053fb15e0165483f74671f5352ebb4681dda40
rudrajiii <rudrasaha305@gmail.com>
2024-10-26 14:03:11 +0530

    slice done 🚀

---
+++ 9_slice/index.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"slices"
+)
+
+//* slice -> dynamic arrays
+//* most used construct in go
+//* useful methods
+func main(){
+	//* uninitialized slice in nil
+	var num[]int
+	fmt.Println(num)
+	fmt.Println(num == nil)
+	fmt.Println(len(num))
+
+	//* capacity -> maximum numbers of elements can fit
+	var nums = make([]int,2,5)
+	fmt.Println(cap(nums))
+	fmt.Println(nums)
+
+	nums[0] = 3
+	nums[1] = 4
+
+	nums = append(nums, 1)
+	nums = append(nums, 2)
+	nums = append(nums, 3)
+	nums = append(nums, 4)
+	fmt.Println(cap(nums))
+	fmt.Println(nums)
+	fmt.Println(len(nums))
+
+	arr := []int{}
+	arr = append(arr, 1)
+	fmt.Println(arr)
+
+	//* copy func
+	var nums1 = make([]int , 0 , 5)
+	nums1 = append(nums1, 2)
+	var nums2 = make([]int , len(nums1))
+
+	copy(nums2 , nums1)
+
+	fmt.Println(nums1 , nums2)
+
+	//* slice operator
+	a := []int{1,2,3}
+	fmt.Println(a[:])
+
+	//* slice func
+	a1 := []int{1,2}
+	a2 := []int{1,2,3}
+	fmt.Println(slices.Equal(a1 , a2))
+
+	D_2 := [][]int{{1,2},{2,3}}
+	fmt.Println(D_2)
+
+}
+
---
+++ Makefile
@@ -0,0 +1,23 @@
+basic:
+	go run 1_hello_world/first.go
+value:
+	@go run 2_values/index.go
+var:
+	@go run 3_simple_variables/index.go
+const:
+	@go run 4_constants/index.go
+loop:
+	@go run 5_forloop/index.go
+cond:
+	@go run 6_if-else/index.go
+switch:
+	@go run 7_switch/index.go
+arr:
+	@go run 8_Arrays/index.go
+slice:
+	@if [ -f 9_slice/index.go ]; then \
+		go run 9_slice/index.go; \
+	else \
+		echo "Error: 9_slice/index.go does not exist."; \
+	fi
+
